781856d4d5107ec7f6b1e139a570d1e10df43906
Leandro Núñez <leandro161996@gmail.com>
2020-07-25 00:59:57 -0400

    Hora y timezone del sistema añadida. Se corrige rut de profesor en seeder. Identacion de layout para profesor.

--- database/seeds/ProfesorSeeder.php
+++ database/seeds/ProfesorSeeder.php
@@ -4,21 +4,21 @@ use Illuminate\Database\Seeder;
 
 class ProfesorSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('profesores')->insert([
-            'rut' => '1.111.111-1',
-            'nombres' => 'Julio',
-            'apellidos' => 'Profe',
-            'fnac' => '2020-01-01',
-            'telefono' => '123123123',
-            'email' => 'prueba@prueba.com',
-            'password' => bcrypt('123123')
-        ]);
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('profesores')->insert([
+			'rut' => '11111111-1',
+			'nombres' => 'Julio',
+			'apellidos' => 'Profe',
+			'fnac' => '2020-01-01',
+			'telefono' => '123123123',
+			'email' => 'prueba@prueba.com',
+			'password' => bcrypt('123123')
+		]);
+	}
 }
