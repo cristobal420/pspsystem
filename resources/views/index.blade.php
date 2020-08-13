98ad101572a27d9e387a17d309e45c1a98f78182
Leandro Núñez <leandro161996@gmail.com>
2020-08-13 01:23:44 -0400

    Se añade la ultima version de bootstrap. Se crean las dos columnas para el inicio, para separar los contenidos.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>INICIO - PSPSYSTEM</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('bootstrap-4.5.2-dist/css/bootstrap.min.css') }}">
+	<!-- Google Font: Source Sans Pro -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+	<!-- Font Awesome Icons -->
+	{{-- <link rel="stylesheet" href="{{ asset('AdminLte/plugins/fontawesome-free/css/all.min.css') }}"> --}}
+	<!-- Theme style -->
+	{{-- <link rel="stylesheet" href="{{ asset('AdminLte/css/adminlte.min.css') }}"> --}}
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-md-5 mx-auto" style="background-color: green;">
+				<div class="container"></div>
+			</div>
+			<div class="col-12 col-md-2 border" style="background-color: black;"></div>
+			<div class="col-12 col-md-5 mx-auto" style="background-color: purple;"></div>
+		</div>
+	</div>
+
+</body>
+</html>
