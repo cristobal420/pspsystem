66edbe386541ac1b3bd267a780bd36c41180208a
Leandro Núñez <leandro161996@gmail.com>
2020-09-27 23:32:37 -0300

    Nuevo auth para alumnos. Nuevo controlador vacio. Modelo de alumnos con caracteristicas para inicio de sesión.

--- app/Alumnos.php
+++ app/Alumnos.php
@@ -4,12 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Alumnos extends Model
+class Alumnos extends Authenticatable
 {
 	use SoftDeletes;
-    protected $table='alumnos';
+	protected $table='alumnos';
 	protected  $guarded = [];
+
 	public function profesores()
 	{
 		return $this->belongsTo('App\Profesores');
