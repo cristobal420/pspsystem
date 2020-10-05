ffbf2f1d31071bf813e8b0636c6d0218f00e4a03
Leandro Núñez <leandro161996@gmail.com>
2020-10-05 08:56:18 -0300

    Nuevo controlador vacío, con pautas a seguir para el avance del proyecto.

---
+++ app/Http/Controllers/ActividadesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ActividadesController extends Controller
+{
+	/* ##### OBJETIVOS ##### */
+	// PROFE
+	// ASIGNAR ACTIVIDADS A ALUMNOS.
+	// Recomendar actividades segun categorias/edades.
+	// Enviar correos de actividades asignadas.
+	// Alumno
+	// Realizar actividades.
+	// Guardar respuestas, resultados, etc.
+	// Generar y enviar reportes al alumno.
+	/* ##### OBJETIVOS ##### */
+}
