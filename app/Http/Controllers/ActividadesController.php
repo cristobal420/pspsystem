6f87b24d33f943ca93f9c02742da9ce7906bb6d1
Leandro Núñez <leandro161996@gmail.com>
2020-10-11 01:16:29 -0300

    Se añaden clases a los controladores.

--- app/Http/Controllers/ActividadesController.php
+++ app/Http/Controllers/ActividadesController.php
@@ -3,6 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Alumnos;
+use App\Profesores;
+use App\Categorias;
+use App\Subcategorias;
+use App\Niveles;
+use App\Actividades;
+use App\Preguntas;
+use App\Respuestas;
+use Flash;
+use Auth;
+use Mail;
 
 class ActividadesController extends Controller
 {
